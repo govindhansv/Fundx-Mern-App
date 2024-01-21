97c1362b49326b507bb9b05484f91c8f6795cd31
gokulhansv <gokulhansv@gmail.com>
2024-01-21 19:31:33 +0530

    initial

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./db");
+require("dotenv").config();
+
+app.use(express.json());
+
+app.use(cors());
+
+const userRouter = require("./routes/userRoutes");
+
+app.use("/api/user", userRouter);
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Server is running on port 5000")
+);
